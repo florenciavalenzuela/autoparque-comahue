169c14397587559dc8de84d087dc1adc6a922919
Flor Valenzuela <floor.valenzuela@hotmail.com>
2023-10-28 23:59:06 -0300

    .

---
+++ src/Components/ComponenteA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ComponenteB from './ComponenteB'
+
+const ComponenteA = () => {
+  return (
+    <>
+      <div>ComponenteA</div>
+      <ComponenteB/>
+    </>
+  )
+}
+
+export default ComponenteA
