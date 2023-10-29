065131a28b1f3f591b2e7382f7c36ce2e8d97e85
Flor Valenzuela <floor.valenzuela@hotmail.com>
2023-10-29 00:37:58 -0300

    .

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
+        <div>
+            <h3>Autoparque Comahue</h3>
+            <Menu>
+                <MenuButton>
+                    Categories
+                </MenuButton>
+                <MenuList>
+                    <MenuItem>Download</MenuItem>
+                    <MenuItem>Create a Copy</MenuItem>
+                    <MenuItem>Mark as Draft</MenuItem>
+                    <MenuItem>Delete</MenuItem>
+                    <MenuItem>Attend a Workshop</MenuItem>
+                </MenuList>
+            </Menu>
+        </div>
+    )
 }
 
 export default Navbar
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react'
+import {  ChakraProvider } from '@chakra-ui/react'
 
 
 
@@ -9,5 +9,5 @@ import { ChakraBaseProvider, ChakraProvider } from '@chakra-ui/react'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
     <App />
-  </ChakraProvider>,
+  </ChakraProvider>
 )
